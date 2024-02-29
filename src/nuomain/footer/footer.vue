ca0212f97e64a3f38b1caf2aa55b6cb728cd32dd
Xu Weikang <78991709+nuoyis@users.noreply.github.com>
2024-02-29 17:23:06 +0800

    update

--- src/nuomain/footer/footer.vue
+++ src/nuomain/footer/footer.vue
@@ -4,9 +4,9 @@
     |
     备案号:<a href="//beian.miit.gov.cn" data-pjax-state="">鄂ICP备2023004941号-2</a>
     |
-    <img style="vertical-align:middle" src="./img/beian.png" data-ll-status="loading" class="entered loading">鄂公网安备<a href="//www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011702000728" data-pjax-state="">42011702000728</a>号
+    <img style="vertical-align:middle" src="@/img/beian.png" data-ll-status="loading" class="entered loading">鄂公网安备<a href="//www.beian.gov.cn/portal/registerSystemInfo?recordcode=42011702000728" data-pjax-state="">42011702000728</a>号
     |
-    © 2020 - {{ nowdate }}<a href="https://www.nuoyis.com" target="_blank" style="color:#999;">  诺依阁</a>
+    © 2020 - {{ nowdate }}<a href="https://www.nuoyis.net" target="_blank" style="color:#999;">  诺依阁</a>
   </div>
 </template>
 
